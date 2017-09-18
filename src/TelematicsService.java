e6a9002bcf608d175dc0fb538bb16095cdbc807a
Evan Coakley <evancoakley1@gmail.com>
2017-09-18 08:30:42 -0500

    teleService functions again, needs othere work done from directions

--- src/TelematicsService.java
+++ src/TelematicsService.java
@@ -8,26 +8,26 @@ import java.io.IOException;
 
 public class TelematicsService {
 
-     void report(VehicleInfo vehicleInfo) {
-
-         ObjectMapper mapper = new ObjectMapper();
-         try {
-             String json = mapper.writeValueAsString(vehicleInfo);
-
-             try {
-                 File file = new File(Integer.toString(vehicleInfo.getVIN()) + ".json");
-                 FileWriter fileWriter = new FileWriter(file);
-                 fileWriter.write(json);
-                 fileWriter.close();
-             }catch (JsonProcessingException e) {
-                 e.printStackTrace();
-             } catch (IOException ex) {
-                 ex.printStackTrace();
-             } finally {
-                 System.out.println("You are done");
-             }
+    static void report(VehicleInfo vehicleInfo) {
 
 
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String json = mapper.writeValueAsString(vehicleInfo);
 
-     }
+            try {
+                File file = new File(Integer.toString(vehicleInfo.getVIN()) + ".json");
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(json);
+                fileWriter.close();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }catch (IOException ex) {
+            ex.printStackTrace();
+//        } finally {
+//            System.out.println("You are done");
+        }
     }
+
+}
